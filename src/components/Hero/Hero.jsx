995a8f22a569defc89c7a9569f08f7634dc2f3cd
Vamshi Krishna Reddy <pvkr105@gmail.com>
2025-01-20 17:51:44 -0500

    Under Construction landing page for the website

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,14 @@ const Hero = () => {
     <div className="hero container">
       <div className="hero-text">
         <h1>WELCOME TO Desi Grand Rapids Hub</h1>
-        <p>Where you can find all the information you need</p>
+        <h2>🚧 Website Under Construction 🚧</h2>
+        <p>
+          Hello and welcome! 👋 We’re busy building something exciting for our
+          community—a space where connections, rides, and resources come
+          together effortlessly. 🚗✨ Stay tuned! Our new website is coming soon
+          to make your experience even better. Thank you for your patience and
+          support. See you on the road soon!
+        </p>
         <button className="btn">Learn more</button>
       </div>
     </div>
