5bf177f7956932e8a4d36f99880ac610940298a4
Vamshi Krishna Reddy <pvkr105@gmail.com>
2025-01-21 00:30:19 -0500

    add Services component with styling and update Hero button to include icon

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Hero.css";
+import arrow_right from "../../assets/arrow_right.png";
 
 const Hero = () => {
   return (
@@ -8,13 +9,15 @@ const Hero = () => {
         <h1>WELCOME TO Desi Grand Rapids Hub</h1>
         <h2>🚧 Website Under Construction 🚧</h2>
         <p>
-          Hello and welcome! 👋 We’re busy building something exciting for our
+          Hello and welcome! 👋 We are busy building something exciting for our
           community—a space where connections, rides, and resources come
           together effortlessly. 🚗✨ Stay tuned! Our new website is coming soon
           to make your experience even better. Thank you for your patience and
           support. See you on the road soon!
         </p>
-        <button className="btn">Learn more</button>
+        <button className="btn">
+          Learn more <img src={arrow_right} alt=""></img>
+        </button>
       </div>
     </div>
   );
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Services.css";
+
+const Services = () => {
+  return (
+    <div className="services">
+      <div className="service">
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Services;
